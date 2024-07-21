81060e734b7f209c0d328a34e53972c8087fdc11
shagi09 <shalomwubu024@gmail.com>
2024-07-21 18:50:37 +0300

    views created

---
+++ recipe-node/index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const mongoose=require('mongoose');
+
+const app=express()
+
+app.use(express.static('public'))
+
+app.set('view engine','ejs')
+
+const url='mongodb://127.0.0.1:27017/node-auth'
+mongoose.connect(url).then((result)=>{
+    app.listen(4000,()=>{
+        console.log('app listening on port 4000')
+    })
+
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+app.get('/smoothies',(req,res)=>{
+    res.render('smoothies');
+})
